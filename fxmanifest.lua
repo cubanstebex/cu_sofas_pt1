27a877d63cd34a13ca8a402aa3123939f1065961
cubanstebex <cubanstebex@gmail.com>
2026-04-25 14:31:32 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game { 'gta5' }
+author 'evelynsx'
+description 'Cubans 2026 Fivem Furniture pack | Sofas |'
+version '1.0'
+-- https://discord.gg/cubanss for any questions or concerns'
+
+this_is_a_map 'yes'
+
+files {
+    'stream/CUBANS2026V1SOFAS.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/CUBANS2026V1SOFAS.ytyp'
+
+server_scripts {
+    'server.lua',
+    'version.lua',
+}
+
+lua54 'yes'
+
+dependency '/assetpacks'
